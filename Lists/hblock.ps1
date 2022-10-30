06c682ca3d4b52e76eee81b6d45252fcce5356f4
tunisiano187 <fabian@bowlman.org>
2022-10-30 19:54:28 +0100

    First test

--- Lists/hblock.ps1
+++ Lists/hblock.ps1
@@ -1,1 +1,6 @@
-﻿
+﻿$url = "https://hblock.molinero.dev/hosts_adblock.txt"
+$working = "list_$($myInvocation.ScriptName).txt"
+
+(Invoke-WebRequest -Uri $url -split '
+' | Where-Object {$_ -notmatch "!"}).replace('|','').replace('^','') | Set-Content -Path $working
+
--- start.ps1
+++ start.ps1
@@ -3,3 +3,18 @@ Get-ChildItem -Path .\Setup -Filter "*.ps1" | ForEach-Object { . $_.FullName }
 
 Write-Information "Downloading Lists"
 Get-ChildItem -Path .\Lists -Filter "*.ps1" | ForEach-Object { . $_.FullName }
+
+Write-Information "Commit changes"
+$toadd = (get-childitem -path "." -Filter "list*.txt").FullName
+foreach ($file in $toadd) {
+    git add $file
+}
+git commit -m "add lists"
+
+Write-Information "Push if required"
+try {
+    git pull
+    git push origin master
+} catch {
+    write-output "nothing to push"
+}
