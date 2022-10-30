ceec3368b9cfa3aca9b6260205c8222835063213
tunisiano187 <fabian@bowlman.org>
2022-10-30 19:57:51 +0100

    .split

--- Lists/hblock.ps1
+++ Lists/hblock.ps1
@@ -1,6 +1,6 @@
 ﻿$url = "https://hblock.molinero.dev/hosts_adblock.txt"
 $working = "list_$($myInvocation.ScriptName).txt"
 
-(Invoke-WebRequest -Uri $url -split '
-' | Where-Object {$_ -notmatch "!"}).replace('|','').replace('^','') | Set-Content -Path $working
+((Invoke-WebRequest -Uri $url).split('
+') | Where-Object {$_ -notmatch "!"}).replace('|','').replace('^','') | Set-Content -Path $working
 
