7e144b6eb56ccfe8f727f4e6274ce7a4bb4c45c9
tunisiano187 <fabian@bowlman.org>
2022-10-30 18:53:05 +0100

    PS4GH

---
+++ Setup/PSForGithub.ps1
@@ -0,0 +1,10 @@
+﻿# Install required tools for this repository and setup required variables
+    Install-Module -Name PowerShellForGitHub
+    $secureString = ${{ secrets.GITHUB_TOKEN }} | ConvertTo-SecureString -AsPlainText -Force
+    $cred = New-Object System.Management.Automation.PSCredential "username is ignored", $secureString
+    Set-GitHubAuthentication -Credential $cred
+    $secureString = $null # clear this out now that it's no longer needed
+    $cred = $null # clear this out now that it's no longer needed
+
+
+    Get-GitHubUser -Current
--- start.ps1
+++ start.ps1
@@ -1,1 +1,5 @@
-﻿Write-host "First test of the automation"
+﻿Write-Information "Setting up the workflow"
+Get-ChildItem -Path .\Setup -Filter "*.ps1" | ForEach-Object { . $_.FullName }
+
+Write-Information "Downloading Lists"
+Get-ChildItem -Path .\Lists -Filter "*.ps1" | ForEach-Object { . $_.FullName }
