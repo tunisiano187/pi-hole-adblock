9903b5a97875abc0593fbba657f23a84f9d8bc6d
tunisiano187 <fabian@bowlman.org>
2022-10-30 20:05:31 +0100

    tostring

--- Lists/hblock.ps1
+++ Lists/hblock.ps1
@@ -1,7 +1,7 @@
 ﻿$url = "https://hblock.molinero.dev/hosts_adblock.txt"
 $working = "list_$($myInvocation.ScriptName).txt"
 
-$content = Invoke-WebRequest -Uri $url
-($content.split('
+$content = (Invoke-WebRequest -Uri $url)
+($content.Tostring().split('
 ') | Where-Object {$_ -notmatch "!"}).replace('|','').replace('^','') | Set-Content -Path $working
 
